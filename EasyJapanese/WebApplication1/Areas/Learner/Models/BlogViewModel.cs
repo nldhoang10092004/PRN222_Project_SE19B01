16aea9f92ea7370306b727082e8c29be6c421618
Tra <nguyenthithanhtra1808@gmail.com>
2026-06-20 09:10:09 +0700

    Learner dashboard

---
+++ EasyJapanese/WebApplication1/Areas/Learner/Models/BlogViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreWeb.Areas.Learner.Models
+{
+    public class CommunityIndexViewModel
+    {
+        public string FullName { get; set; } = string.Empty;
+    }
+}
