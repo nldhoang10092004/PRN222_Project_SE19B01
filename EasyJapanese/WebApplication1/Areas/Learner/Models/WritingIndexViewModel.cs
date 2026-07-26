1c200f4e37f9d23ac24edc0a504848baecddf927
Tra <nguyenthithanhtra1808@gmail.com>
2026-07-26 23:18:56 +0700

    Create writing with AI

---
+++ EasyJapanese/WebApplication1/Areas/Learner/Models/WritingIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Areas.Learner.Models
+{
+    public class WritingIndexViewModel
+    {
+        public string EssayLevel { get; set; } = "";
+        public string EssayTopic { get; set; } = "";
+        public string TranslationLevel { get; set; } = "";
+        public string TranslationSentence { get; set; } = "";
+    }
+
+    public class EssayGradeRequest
+    {
+        public string Topic { get; set; } = "";
+        public string Level { get; set; } = "";
+        public string StudentText { get; set; } = "";
+    }
+
+    public class TranslationGradeRequest
+    {
+        public string VietnameseSentence { get; set; } = "";
+        public string Level { get; set; } = "";
+        public string StudentText { get; set; } = "";
+    }
+}
