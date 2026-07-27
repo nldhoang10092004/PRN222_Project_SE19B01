be30cf7d5e5ef369ef0d309b353e0ffd9f46e329
Nguyen Le Duy Hoang <nldhoang10092004@gmail.com>
2026-07-27 08:21:42 +0700

    add const

---
+++ EasyJapanese/CoreLibrary/Const/DifficultyConst.cs
@@ -0,0 +1,14 @@
+namespace CoreLibrary.Const
+{
+    public static class DifficultyConst
+    {
+        /// <summary>Dễ</summary>
+        public const int EASY = 1;
+
+        /// <summary>Trung bình</summary>
+        public const int MEDIUM = 2;
+
+        /// <summary>Khó</summary>
+        public const int HARD = 3;
+    }
+}
